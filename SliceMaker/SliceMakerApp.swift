f6931eec0b4580d0179bd9457b76d34fa00006f6
IvanMagda-energ <73852159+IvanMagda-energ@users.noreply.github.com>
2025-08-04 09:03:25 +0300

    Initial Commit

---
+++ SliceMaker/SliceMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliceMakerApp.swift
+//  SliceMaker
+//
+//  Created by Ivan Magda on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SliceMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SliceMakerTests/SliceMakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SliceMakerTests.swift
+//  SliceMakerTests
+//
+//  Created by Ivan Magda on 04.08.2025.
+//
+
+import Testing
+
+struct SliceMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
