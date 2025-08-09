f12ad930973c76e3387ddf3323c241bc3ec8f164
IvanMagda-energ <73852159+IvanMagda-energ@users.noreply.github.com>
2025-08-09 22:47:53 +0300

    feat: Add custom rule to swift lint configuration file

--- SliceMaker/ContentView.swift
+++ SliceMaker/ContentView.swift
@@ -9,22 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(makeAttributed())
-            .environment(\.openURL, OpenURLAction(handler: handleURL))
-    }
-    
-    func makeAttributed() -> AttributedString {
-        var text = AttributedString("Tap here to learn more.")
-        if let range = text.range(of: "here") {
-            text[range].link = URL(string: "")
-            text[range].foregroundColor = .blue
-        }
-        return text
-    }
-    
-    func handleURL(_ url: URL) -> OpenURLAction.Result {
-        print("Handle \(url) somehow")
-        return .handled
+        Text("Hello World!")
     }
 }
 
